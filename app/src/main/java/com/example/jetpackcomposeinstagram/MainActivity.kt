e209b6e5cfdf5c2c470dafdc920ee1507e413893
Ivan <icarpiodeveloper@gmail.com>
2024-09-03 21:38:59 +0200

    Add ModalNavigationDrawer

--- app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import com.example.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
@@ -20,20 +24,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeInstagramTheme {
-                // Convertir el Color de Jetpack Compose a un color entero (Int)
-                //window.statusBarColor = colorResource(id = R.color.teal_200).toArgb()
                 val snackbarHostState = remember { SnackbarHostState() }
                 val scope = rememberCoroutineScope()
-                Scaffold(
-                    topBar = { MyTopAppBar { scope.launch { snackbarHostState.showSnackbar("Has pulsado $it") } } },
-                    snackbarHost = { SnackbarHost(snackbarHostState) },
-                    bottomBar = { MyBottomNavigation()}
+                val drawerState = rememberDrawerState(DrawerValue.Closed)
 
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            MyModalDrawer {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            }
+                        }
+                    },
+                    drawerState = drawerState
                 ) {
+                    Scaffold(
+                        topBar = {
+                            MyTopAppBar {
+                                scope.launch {
+                                    snackbarHostState.showSnackbar("Has pulsado $it")
+                                }
+                            }
+                        },
+                        snackbarHost = { SnackbarHost(snackbarHostState) },
+                        bottomBar = { MyBottomNavigation() }
+                    ) {
+                        // Aquí puedes agregar contenido principal
+                    }
                 }
             }
         }
     }
 }
-
-
--- app/src/main/java/com/example/jetpackcomposeinstagram/Scaffold.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/Scaffold.kt
@@ -1,27 +1,38 @@
 package com.example.jetpackcomposeinstagram
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,7 +48,7 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
             ),
         navigationIcon = {
             IconButton(onClick = { onClickIcon("Atrás") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Atrás")
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Atrás")
             }
         },
         actions = {
@@ -96,4 +107,33 @@ fun MyBottomNavigation() {
     }
 }
 
+@Composable
+fun MyModalDrawer(onCloseDrawer: () -> Unit) {
+    Column(Modifier.padding(8.dp)) {
+        Text(
+            text = "Primera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Segunda opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Tercera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Cuarta opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .clickable { onCloseDrawer() }
+        )
+    }
+}
 
